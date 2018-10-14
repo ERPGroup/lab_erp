72ab7ba584dd06bc7ac5f46580a529e3a0e6a456
DefTnT <jimmi2051@gmail.com>
2018-10-15 00:03:58 +0700

    Merchant

--- merchant/urls.py
+++ merchant/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
    path('login/',views.login,name='login'),
    path('manager_product',views.product,name='product'),
    path('manager_product_detail',views.product_detail,name='product'),
+   path('manager_post',views.posted,name='product'),
+   path('manager_post_detail',views.posted_detail,name='product'),
+   path('manager_warehose',views.warehose,name='warehose'),
 ]
--- merchant/views.py
+++ merchant/views.py
@@ -6,6 +6,12 @@ def login (request):
 def index(request):
     return render(request,'merchant/index.html')
 def product(request):
-    return render(request,'admin/manager_product/manager_product.html')
+    return render(request,'merchant/manager_product/manager_product.html')
 def product_detail(request):
-    return render(request,'admin/manager_product/manager_product_detail.html')
+    return render(request,'merchant/manager_product/manager_product_detail.html')
+def posted(request):
+    return render(request,'merchant/manager_posted/manager_post.html')
+def posted_detail(request):
+    return render(request,'merchant/manager_posted/manager_post_detail.html')
+def warehose(request):
+    return render(request,'merchant/manager_product/manager_warehose.html')
