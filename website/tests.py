2d13775eaeefc0503f94ac7c46e81d4881e305c5
dinh truong <dinhtruong018@gmail.com>
2018-10-28 13:58:25 +0700

    Add testcase

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from website.models import Category
 # Create your tests here.
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name_category="Sach", quantity=2)
+        
+
+    def test_sach(self):
+        sach = Category.objects.get(name_category="Sach")
+        self.assertEqual(sach.quantity, 2)
